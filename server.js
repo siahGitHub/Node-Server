f50080692482111c82ce1fad08a666425cd68429
Siah Monk <dwaynemhinds@gmail.com>
2018-07-30 19:03:42 -0400

    updating files

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 const app = express();
-var PORT = 3000;
+//grab the port number for heroku
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
